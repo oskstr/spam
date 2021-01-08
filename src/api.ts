ed967ed8c67fd471c989f32562f14bb7ba7aba8a
Oskar Strömberg <oskstr@kth.se>
2021-01-08 20:13:13 +0100

    Update dist src locations

--- src/api.ts
+++ src/api.ts
@@ -4,11 +4,6 @@ import nodemailer from 'nodemailer';
 import Email from 'email-templates';
 import MarkdownIt from 'markdown-it';
 import Mail from 'nodemailer/lib/mailer';
-import {SendEmailResponse} from "aws-sdk/clients/ses";
-import {PromiseResult} from "aws-sdk/lib/request";
-
-const aws = require("aws-sdk");
-const ses = new aws.SES();
 
 interface EmailRequest {
     to:   string | Mail.Address | Array<string | Mail.Address>;
@@ -51,52 +46,24 @@ export const sendMail = async ({ body }: { body: string }): Promise<APIGatewayPr
 
     const {to, cc, bcc, from , replyTo, subject, content, template} = email
 
-    // return await mail.send({
-    //     message: {
-    //         from,
-    //         replyTo: replyTo || from,
-    //         subject,
-    //         to,
-    //         attachments: [],
-    //     },
-    //     template: template || 'default',
-    //     locals: {
-    //         content: md.render(content),
-    //         raw_content: content
-    //     }
-    // })
-    //     .then((status: any) => {
-    //         console.info('status', status)
-    //         return {
-    //             statusCode: 200,
-    //             body: JSON.stringify(status)
-    //         }
-    //     })
-    //     .catch((error: AWSError) => {
-    //         console.error('error', error)
-    //         return {
-    //             statusCode: error.statusCode || 500,
-    //             body: JSON.stringify(error)
-    //         }
-    //     })
-
-    const params = {
-        Destination: {
-            ToAddresses: [to],
+    return await mail.send({
+        message: {
+            from,
+            replyTo: replyTo || from,
+            subject,
+            to,
+            cc,
+            bcc,
+            attachments: [],
         },
-        Message: {
-            Body: {
-                Text: { Data: content },
-            },
-
-            Subject: { Data: subject },
-        },
-        Source: from,
-    };
-
-    return await ses.sendEmail(params).promise()
-        .then((status: PromiseResult<SendEmailResponse, AWSError>) => {
-            console.log('status2', status)
+        template: template || 'default',
+        locals: {
+            content: md.render(content),
+            raw_content: content
+        }
+    })
+        .then((status: any) => {
+            console.info('status', status)
             return {
                 statusCode: 200,
                 body: JSON.stringify(status)
